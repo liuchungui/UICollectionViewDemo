44392afb6ab7a6945525c4faa356042ec798112b
chunguiLiu <404468494@qq.com>
2015-11-02 00:26:05 +0800

    add demo

---
+++ UICollectionViewDemo/UICollectionViewDemo1/UICollectionViewDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewDemo1
+//
+//  Created by user on 15/11/1.
+//  Copyright © 2015年 BG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewDemo/UICollectionViewDemo1/UICollectionViewDemo1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UICollectionViewDemo1
+//
+//  Created by user on 15/11/1.
+//  Copyright © 2015年 BG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
